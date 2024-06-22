dc9e6c42f786711187813ac5582c2a03d1e679e3
Noah <emredaglier@gmail.com>
2024-06-22 13:51:06 +0300

    blog improvements

---
+++ public/blog/script.js
@@ -0,0 +1,29 @@
+const menuItemAll = document.getElementById("menu-blog-all");
+const menuItemAero = document.getElementById("menu-blog-aero");
+const menuItemTech = document.getElementById("menu-blog-tech");
+const menuItemCode = document.getElementById("menu-blog-code");
+const menuItemArt = document.getElementById("menu-blog-art");
+const menuItemTravel = document.getElementById("menu-blog-travel");
+const menuItemWork = document.getElementById("menu-blog-work");
+
+document.addEventListener('DOMContentLoaded', () => {
+    const navItems = document.querySelectorAll('.menuJS');
+    const pageItems = document.querySelectorAll('.divJS');
+
+    navItems.forEach((item, index) => {
+        item.addEventListener('click', () => {
+            // Remove the 'selected' class from all items
+            navItems.forEach(i => i.classList.remove('text-[#05AFF2]'));
+            navItems.forEach(i => i.classList.add('text-white'));
+
+            // Add the 'selected' class to the clicked item
+            item.classList.remove('text-white');
+            item.classList.add('text-[#05AFF2]');
+
+            pageItems.forEach(i => i.classList.remove('flex'));
+            pageItems.forEach(i => i.classList.add('hidden'));
+            pageItems[index].classList.add('flex');
+            pageItems[index].classList.remove('hidden');
+        });
+    });
+});

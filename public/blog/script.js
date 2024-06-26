98fd6d704336a3705cf0541092a574635e1c87ce
Noah <emredaglier@gmail.com>
2024-06-26 21:26:10 +0300

    blog menu fixed

--- public/blog/script.js
+++ public/blog/script.js
@@ -28,15 +28,3 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
-const pointerScroll = (elem) => {
-
-    const dragStart = (ev) => elem.setPointerCapture(ev.pointerId);
-    const dragEnd = (ev) => elem.releasePointerCapture(ev.pointerId);
-    const drag = (ev) => elem.hasPointerCapture(ev.pointerId) && (elem.scrollLeft -= ev.movementX);
-
-    elem.addEventListener("pointerdown", dragStart);
-    elem.addEventListener("pointerup", dragEnd);
-    elem.addEventListener("pointermove", drag);
-};
-
-document.querySelectorAll(".grab-scroll").forEach(pointerScroll);
